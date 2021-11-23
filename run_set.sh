b8c25d95e657265c15362702ae0e59c3dd02c72c
Hernan Campos <hernan.campos@mpimet.mpg.de>
2021-11-23 13:30:37 +0100

    more runs done, more logfiles

--- run_set.sh
+++ run_set.sh
@@ -6,16 +6,17 @@
 # --------------------------------
 #SBATCH --account=mh0926
 
-#SBATCH --job-name=exp.python_speed_test.run
+#SBATCH --job-name=matmul_test
 #SBATCH --partition=compute2,compute
-#SBATCH --chdir=/work/mh0926/m300872/git/speed6/build/run
+#SBATCH --partition=compute
+#SBATCH --chdir=/pf/m/m300872/matmul_benchmark
 #SBATCH --nodes=1
 #SBATCH --threads-per-core=2
 # the following is needed to work around a bug that otherwise leads to
 # a too low number of ranks when using compute,compute2 as queue
 #SBATCH --mem=0
-#SBATCH --output=/work/mh0926/m300872/git/speed6/standalone_speedtest/log/run_set%j.log
-#SBATCH --error=/work/mh0926/m300872/git/speed6/standalone_speedtest/log/run_set%j.log
+#SBATCH --output=/pf/m/m300872/matmul_benchmark/log/run_set%j.log
+#SBATCH --error=/pf/m/m300872/matmul_benchmark/log/run_set%j.log
 #SBATCH --exclusive
 #SBATCH --time=08:00:00
 
@@ -30,6 +31,10 @@ module load python3
 compiler="gfortran ifort"
 timer="OMP_WTIME ETIME_ELAPSED ETIME_USER ETIME_SYSTEM ICON_WALLCLOCK_TIMER TIMER_CPU_TIME"
 optimisation="2 3"
+
+compiler="gfortran"
+timer="OMP_WTIME ETIME_ELAPSED ETIME_USER ETIME_SYSTEM ICON_WALLCLOCK_TIMER TIMER_CPU_TIME"
+optimisation="3"
 
 # # light weight test set
 # compiler="gfortran"
